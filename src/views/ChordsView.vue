ee2dfa371ba11f80bfc2d30dd82d4b98f89299b4
Ane Jørgensen <anesoejoe@gmail.com>
2022-04-24 20:26:53 +0200

    Added some data

--- src/views/ChordsView.vue
+++ src/views/ChordsView.vue
@@ -9,7 +9,14 @@
 import ButtonComponent from '@/components/ButtonComponent'
 export default {
   name: 'ChordsView',
-  components: { ButtonComponent }
+  components: { ButtonComponent },
+  data () {
+    return {
+      minorChords: ['Am', 'Bbm', 'Bm', 'Cm', 'C#m', 'Dm', 'D#m', 'Em', 'Fm', 'F#m', 'Gm', 'G#m'],
+      majorChords: ['A', 'Bb', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'],
+      inversions: ['Root', '1st', '2nd']
+    }
+  }
 }
 </script>
 
